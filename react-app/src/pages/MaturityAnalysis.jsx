bd1b5a2cc6adbeb0d61fbfff9a08660044b31824
qiqi-zhou77 <71381895+qiqi-zhou77@users.noreply.github.com>
2023-08-04 00:35:18 +0200

    Import date picker function

--- react-app/src/pages/MaturityAnalysis.jsx
+++ react-app/src/pages/MaturityAnalysis.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from '../components/Sidebar';
+import Calendar from '../components/Calendar';
 import "../App.css";
 
 const MaturityAnalysis = () => {
+
+
+  const [custom_date, setCustomDate] = useState('');
+
+  const handleUserPickDate = (date) => {
+    if (date) {
+      const formattedDate = `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+      setCustomDate(formattedDate);
+    } else {
+      setCustomDate('');
+    }
+  };
+
   return (
     <div className='background-image'>
+
+      <div> 
         <Sidebar />
+      </div>
+
+      <div className = 'calendar-container'>
+      <div className = 'calendar-content'>
+
+        <h1>Date Picker</h1>
+        <Calendar onDateSelect={handleUserPickDate} />
+        {custom_date && <p>User entered date: {custom_date}</p>}
+    </div>
+    </div>
+
     </div>
   )
 }
