e94e2497e96f765b6a630986ecfe2379d95f1b7d
Qiqi Zhou <qiqi.zhou77@outlook.com>
2023-08-03 16:06:49 +0200

    axiom setting

--- react-app/src/pages/BondsOverview.jsx
+++ react-app/src/pages/BondsOverview.jsx
@@ -3,11 +3,29 @@ import Sidebar from '../components/Sidebar';
 import "../App.css";
 import TableForm from '../components/TableForm';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { getAllSecurities } from '../service/security-service';
 
 const BondsOverview = () => {
 
   // const [contacts, setContacts] = useState(data);
   // const [search, setSearch] = useState('');
+  const [bonds,setBonds] = useState([]);
+
+  useEffect(()=>{
+        getBondsFromAPI();}, 
+        []
+);
+
+const getBondsFromAPI = ()=>{
+  getAllSecurities()
+  .then(res => {
+      setBonds(res.data);
+  })
+  .catch(err => {
+      setBonds([]);
+      console.log(err);
+  })
+}
 
   return (
       <div>   
